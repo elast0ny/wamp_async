f6f391cecf152d9b749f7ac7efbb978eb0f6c1dc
Lucas McGartland <luke.mcgartland@me.com>
2020-11-18 06:51:04 -0800

    feat: upgrade to tokio 0.3 (#12)

--- examples/rpc_callee.rs
+++ examples/rpc_callee.rs
@@ -98,7 +98,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         if call_num >= 4 || !client.is_connected() {
             break;
         }
-        tokio::time::delay_for(std::time::Duration::from_secs(1)).await;
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     }
 
     // Client should not have disconnected
